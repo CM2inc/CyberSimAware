ac8c39226d92dc2e20c238ef3cba121e10357852
djcpu <morga2cd@mail.uc.edu>
2022-12-01 14:21:50 -0500

    first commit

---
+++ Models/ProductListViewModel.cs
@@ -0,0 +1,18 @@
+﻿//Cepada Morgan View Model 
+using System;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Collections.Generic;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+            public List<Category> Categories { get; set; }
+            public List<Product> Products { get; set; }
+            public string SelectedCategory { get; set; }
+            public string CheckActiveCategory(string category) =>
+                category == SelectedCategory ? "active" : "";
+        
+    }
+}
+
